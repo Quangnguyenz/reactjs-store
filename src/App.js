3e607fd0f01288f2bc56f20f2f2a4ce908ff9522
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-12-13 21:55:05 -0600

    Added a few components to the home page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-
-import styled from 'styled-components'
-
-import Testing from './testing'
-
-const Button = styled.button`
-background: green;
-color: white;`
-
-const Container = styled.div`
-background: red;
-color: white;
-font-size: 2rem;
-.hero{
-  font-size: 8rem;
-  background: pink;
-}
-
-`
+import { Home, Products, SingleProduct, About, Cart, Error, Checkout, Private } from './pages'
 
 function App() {
   return (
-    <div>
-      <h4>comfy sloth starter</h4>
-      <Testing />
-    </div>)
+    <Router>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+      <Switch>
+        <Route exact path='/'>
+          <Home></Home>
+        </Route>
+        <Route exact path='/about'>
+          <About></About>
+        </Route>
+        <Route exact path='/cart'>
+          <Cart></Cart>
+        </Route>
+        <Route exact path='/products'>
+          <Products></Products>
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />}>
+        </Route>
+        <Route exact path='/checkout'>
+          <Checkout></Checkout>
+        </Route>
+        <Route exact path='*'>
+          <Error></Error>
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
