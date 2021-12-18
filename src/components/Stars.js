53e79649ea5bfc73d629bb22cd48801da8ed812d
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-12-18 14:46:28 -0600

    Created the add to cart component with its functions and smaller components

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,35 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+import { TimeoutError } from '@auth0/auth0-spa-js'
 const Stars = ({ stars, reviews }) => {
-  console.log(reviews)
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5
+    return (
+      < span key={index}>
+        {stars >= index + 1 ? <BsStarFill /> : stars >= number ? <BsStarHalf /> : <BsStar />
+        }
+      </span >
+    )
+  })
   return <Wrapper>
     <div className='stars'>
-      {/* star */}
-      <span>
-        {stars >= 1 ? <BsStarFill /> : stars >= 5 ? <BsStarHalf /> : <BsStar />}
-      </span>
-      {/* end of star */}
-      {/* star */}
-      <span>
-        {stars >= 2 ? <BsStarFill /> : stars >= 1.5 ? <BsStarHalf /> : <BsStar />}
-      </span>
-      {/* end of star */}
-      {/* star */}
-      <span>
-        {stars >= 3 ? <BsStarFill /> : stars >= 2.5 ? <BsStarHalf /> : <BsStar />}
-      </span>
-      {/* end of star */}
-      {/* star */}
-      <span>
-        {stars >= 4 ? <BsStarFill /> : stars >= 3.5 ? <BsStarHalf /> : <BsStar />}
-      </span>
-      {/* end of star */}
-      {/* star */}
-      <span>
-        {stars === 5 ? <BsStarFill /> : stars >= 4.5 ? <BsStarHalf /> : <BsStar />}
-      </span>
-      {/* end of star */}
+      {tempStars}
     </div>
     <p className='review'>({reviews} customer reviews)</p>
   </Wrapper>
