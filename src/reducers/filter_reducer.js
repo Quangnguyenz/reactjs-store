b379c35513cc0448d1e805f6978a6f73854fb594
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-12-28 19:06:38 -0600

    Added the cart page

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,7 +13,7 @@ const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
     let maxPrice = action.payload.map((p) => p.price)
     maxPrice = Math.max(...maxPrice)
-    return { ...state, all_products: [...action.payload], filtered_products: [...action.payload], filters: { ...state.filters, max_price: maxPrice } }
+    return { ...state, all_products: [...action.payload], filtered_products: [...action.payload], filters: { ...state.filters, max_price: maxPrice, price: maxPrice } }
   }
   if (action.type === SET_GRIDVIEW) {
     return { ...state, grid_view: true }
@@ -72,6 +72,21 @@ const filter_reducer = (state, action) => {
         return product.company === company
       })
     }
+
+    // color
+    if (color !== 'all') {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color)
+      })
+    }
+
+    // price
+    tempProducts = tempProducts.filter((product) => product.price <= price)
+    // shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter((product) => product.shipping === true)
+    }
+
     return { ...state, filtered_products: tempProducts }
   }
   if (action.type === CLEAR_FILTERS) {
