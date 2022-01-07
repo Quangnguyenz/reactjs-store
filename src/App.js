f65591d76d34f224aa5b3f2bf0e7cf59f6f80a00
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-01-07 16:48:35 -0600

    Worked on private route for login user

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import { Home, Products, SingleProduct, About, Cart, Error, Checkout, Private } from './pages'
+import { Home, Products, SingleProduct, About, Cart, Error, Checkout, Private, AuthWrapper } from './pages'
 import PrivateRoute from './pages/PrivateRoute'
 
 function App() {
   return (
-    <Router>
-      <Navbar></Navbar>
-      <Sidebar></Sidebar>
-      <Switch>
-        <Route exact path='/'>
-          <Home></Home>
-        </Route>
-        <Route exact path='/about'>
-          <About></About>
-        </Route>
-        <Route exact path='/cart'>
-          <Cart></Cart>
-        </Route>
-        <Route exact path='/products'>
-          <Products></Products>
-        </Route>
-        <Route exact path='/products/:id' children={<SingleProduct />}>
-        </Route>
-        <PrivateRoute exact path='/checkout'>
-          <Checkout />
-        </PrivateRoute>
-        <Route exact path='*'>
-          <Error></Error>
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar></Navbar>
+        <Sidebar></Sidebar>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/about'>
+            <About></About>
+          </Route>
+          <Route exact path='/cart'>
+            <Cart></Cart>
+          </Route>
+          <Route exact path='/products'>
+            <Products></Products>
+          </Route>
+          <Route exact path='/products/:id' children={<SingleProduct />}>
+          </Route>
+          <PrivateRoute exact path='/checkout'>
+            <Checkout />
+          </PrivateRoute>
+          <Route exact path='*'>
+            <Error></Error>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   )
 }
 
